fb4b6bc851049981408df082d76b65b90d5180d0
RD <research.development@bi-sam.com>
2018-02-16 13:48:56 +0100

    Add Mutator test

--- src/mygeneticalgorithm/Mutator.java
+++ src/mygeneticalgorithm/Mutator.java
@@ -4,22 +4,25 @@ import java.util.Random;
 
 public class Mutator {
 
-    private Random random;
+  private Random random;
 
-    public Mutator(Random random) {
-        this.random = random;
-    }
+  public Mutator(Random random) {
+    this.random = random;
+  }
 
-    public void mutate(Solution solutionToRead, Solution solutionToWrite) {
-        solutionToWrite.containerIndexes = solutionToRead.containerIndexes.clone();
-        int[] containerIndexes = solutionToWrite.containerIndexes;
-        int length = containerIndexes.length;
-        for (int i = 0; i < length / 10; i++) {
-            int i1 = random.nextInt(length);
-            int i2 = random.nextInt(length);
-            int temp = containerIndexes[i1];
-            containerIndexes[i1] = containerIndexes[i2];
-            containerIndexes[i2] = temp;
-        }
+  public void mutate(Solution solutionToRead, Solution solutionToWrite) {
+    solutionToWrite.containerIndexes = solutionToRead.containerIndexes.clone();
+    int[] containerIndexes = solutionToWrite.containerIndexes;
+
+    int length = containerIndexes.length;
+    int[] randomIndex = random.ints(length * 2, 2, length).toArray();
+    for (int i = 0; i < length; i++) {
+      int i1 = randomIndex[2 * i];
+      int i2 = randomIndex[2 * i + 1];
+
+      int temp = containerIndexes[i1];
+      containerIndexes[i1] = containerIndexes[i2];
+      containerIndexes[i2] = temp;
     }
+  }
 }
---
+++ test/mygeneticalgorithm/MutatorTest.java
@@ -0,0 +1,32 @@
+package mygeneticalgorithm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MutatorTest {
+  @Test
+  public void mutate() throws Exception {
+    Random random = new Random();
+    int containerSize = 100;
+    Solution solutionToRead = new Solution(containerSize);
+    IntStream.range(0, containerSize).forEach(i -> solutionToRead.containerIndexes[i] = i);
+    Solution solutionToWrite = new Solution(containerSize);
+
+    Mutator mutator = new Mutator(random);
+    boolean hasMutated = false;
+    for (int j = 0 ; j< 100; j++) {
+      mutator.mutate(solutionToRead, solutionToWrite);
+       hasMutated = hasMutated || IntStream.range(0, containerSize).anyMatch(i -> solutionToRead.containerIndexes[i] != solutionToWrite.containerIndexes[i]);
+      Assert.assertArrayEquals(
+        IntStream.of(solutionToRead.containerIndexes).sorted().toArray(),
+        IntStream.of(solutionToWrite.containerIndexes).sorted().toArray()
+      );
+    }
+    Assert.assertTrue(hasMutated);
+  }
+
+}
